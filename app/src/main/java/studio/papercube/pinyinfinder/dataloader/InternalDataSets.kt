21db3384c940dcd1879ead00792e6facd61c2df9
PaperCube <imzhy@hotmail.com>
2018-04-14 16:06:22 +0800

    add a list for grade 2017

--- app/src/main/java/studio/papercube/pinyinfinder/dataloader/InternalDataSets.kt
+++ app/src/main/java/studio/papercube/pinyinfinder/dataloader/InternalDataSets.kt
@@ -9,7 +9,8 @@ import java.util.*
 enum class InternalDataSets(private val url: String, override val dataName: String) : DataSet {
 //    GRADE2014("/data/grade2014.edt", "2014级"),
     GRADE2015("/data/grade2015.edt", "2015级(beta)"),
-    GRADE2016("/data/grade2016.edt", "2016级");
+    GRADE2016("/data/grade2016.edt", "2016级"),
+    GRADE2017("/data/grade2017.edt", "2017级");
 
     companion object {
         @JvmStatic val TARGET_YEAR = 2017
